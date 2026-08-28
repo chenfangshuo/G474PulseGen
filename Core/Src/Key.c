c7b3dd216ce9f1b56ec84968df725e458b2a57a0
chenfangshuo <chenfangshuo@outlook.com>
2026-08-28 23:04:37 +0800

    perf(key): 优化按键扫描频率并降低TIM7中断开销

--- Core/Src/Key.c
+++ Core/Src/Key.c
@@ -5,8 +5,8 @@
 #define KEY_UNPRESSED			0
 
 #define KEY_TIME_DOUBLE			0
-#define KEY_TIME_LONG			500
-#define KEY_TIME_REPEAT			300
+#define KEY_TIME_LONG			50
+#define KEY_TIME_REPEAT			30
 
 volatile uint8_t Key_Flag[KEY_COUNT];
 
@@ -18,22 +18,14 @@ uint8_t Key_GetState(uint8_t n)
 {
 	switch (n)
 	{
-		case K_UP:
-			return (HAL_GPIO_ReadPin(KEY_UP_GPIO_Port, KEY_UP_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		case K_DOWN:
-			return (HAL_GPIO_ReadPin(KEY_DOWN_GPIO_Port, KEY_DOWN_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		case K_LEFT:
-			return (HAL_GPIO_ReadPin(KEY_LEFT_GPIO_Port, KEY_LEFT_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		case K_RIGHT:
-			return (HAL_GPIO_ReadPin(KEY_RIGHT_GPIO_Port, KEY_RIGHT_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		case K_PRESS:
-			return (HAL_GPIO_ReadPin(KEY_CENTER_GPIO_Port, KEY_CENTER_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		case K_ENC:
-			return (HAL_GPIO_ReadPin(KEY_ENC_GPIO_Port, KEY_ENC_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		case K_TRG:
-			return (HAL_GPIO_ReadPin(KEY_TRG_GPIO_Port, KEY_TRG_Pin) == GPIO_PIN_RESET) ? KEY_PRESSED : KEY_UNPRESSED;
-		default:
-			return KEY_UNPRESSED;
+		case K_UP:    return !(GPIOC->IDR & GPIO_PIN_13);
+		case K_DOWN:  return !(GPIOA->IDR & GPIO_PIN_4);
+		case K_LEFT:  return !(GPIOA->IDR & GPIO_PIN_5);
+		case K_RIGHT: return !(GPIOA->IDR & GPIO_PIN_6);
+		case K_PRESS: return !(GPIOA->IDR & GPIO_PIN_7);
+		case K_ENC:   return !(GPIOB->IDR & GPIO_PIN_0);
+		case K_TRG:   return !(GPIOB->IDR & GPIO_PIN_1);
+		default:      return KEY_UNPRESSED;
 	}
 }
 
@@ -52,11 +44,12 @@ uint8_t Key_Check(uint8_t n, uint8_t Flag)
 
 void Key_Tick(void)
 {
-	static uint8_t Count, i;
+	static uint8_t i;
 	static uint8_t CurrState[KEY_COUNT], PrevState[KEY_COUNT];
 	static uint8_t S[KEY_COUNT];
 	static uint16_t Time[KEY_COUNT];
-	
+
+	/* 递减计时器 */
 	for (i = 0; i < KEY_COUNT; i ++)
 	{
 		if (Time[i] > 0)
@@ -64,90 +57,85 @@ void Key_Tick(void)
 			Time[i] --;
 		}
 	}
-	
-	Count ++;
-	if (Count >= 20)
+
+	/* 每次进入中断直接进行按键扫描与状态机判定 (10ms 一次) */
+	for (i = 0; i < KEY_COUNT; i ++)
 	{
-		Count = 0;
-		
-		for (i = 0; i < KEY_COUNT; i ++)
+		PrevState[i] = CurrState[i];
+		CurrState[i] = Key_GetState(i);
+
+		if (CurrState[i] == KEY_PRESSED)
 		{
-			PrevState[i] = CurrState[i];
-			CurrState[i] = Key_GetState(i);
-			
+			Key_Flag[i] |= KEY_HOLD;
+		}
+		else
+		{
+			Key_Flag[i] &= ~KEY_HOLD;
+		}
+
+		if (CurrState[i] == KEY_PRESSED && PrevState[i] == KEY_UNPRESSED)
+		{
+			Key_Flag[i] |= KEY_DOWN;
+		}
+
+		if (CurrState[i] == KEY_UNPRESSED && PrevState[i] == KEY_PRESSED)
+		{
+			Key_Flag[i] |= KEY_UP;
+		}
+
+		if (S[i] == 0)
+		{
 			if (CurrState[i] == KEY_PRESSED)
 			{
-				Key_Flag[i] |= KEY_HOLD;
+				Time[i] = KEY_TIME_LONG;
+				S[i] = 1;
 			}
-			else
+		}
+		else if (S[i] == 1)
+		{
+			if (CurrState[i] == KEY_UNPRESSED)
 			{
-				Key_Flag[i] &= ~KEY_HOLD;
+				Time[i] = KEY_TIME_DOUBLE;
+				S[i] = 2;
 			}
-			
-			if (CurrState[i] == KEY_PRESSED && PrevState[i] == KEY_UNPRESSED)
+			else if (Time[i] == 0)
 			{
-				Key_Flag[i] |= KEY_DOWN;
+				Time[i] = KEY_TIME_REPEAT;
+				Key_Flag[i] |= KEY_LONG;
+				S[i] = 4;
 			}
-			
-			if (CurrState[i] == KEY_UNPRESSED && PrevState[i] == KEY_PRESSED)
+		}
+		else if (S[i] == 2)
+		{
+			if (CurrState[i] == KEY_PRESSED)
 			{
-				Key_Flag[i] |= KEY_UP;
+				Key_Flag[i] |= KEY_DOUBLE;
+				S[i] = 3;
 			}
-			
-			if (S[i] == 0)
+			else if (Time[i] == 0)
 			{
-				if (CurrState[i] == KEY_PRESSED)
-				{
-					Time[i] = KEY_TIME_LONG;
-					S[i] = 1;
-				}
+				Key_Flag[i] |= KEY_SINGLE;
+				S[i] = 0;
 			}
-			else if (S[i] == 1)
+		}
+		else if (S[i] == 3)
+		{
+			if (CurrState[i] == KEY_UNPRESSED)
 			{
-				if (CurrState[i] == KEY_UNPRESSED)
-				{
-					Time[i] = KEY_TIME_DOUBLE;
-					S[i] = 2;
-				}
-				else if (Time[i] == 0)
-				{
-					Time[i] = KEY_TIME_REPEAT;
-					Key_Flag[i] |= KEY_LONG;
-					S[i] = 4;
-				}
+				S[i] = 0;
 			}
-			else if (S[i] == 2)
+		}
+		else if (S[i] == 4)
+		{
+			if (CurrState[i] == KEY_UNPRESSED)
 			{
-				if (CurrState[i] == KEY_PRESSED)
-				{
-					Key_Flag[i] |= KEY_DOUBLE;
-					S[i] = 3;
-				}
-				else if (Time[i] == 0)
-				{
-					Key_Flag[i] |= KEY_SINGLE;
-					S[i] = 0;
-				}
+				S[i] = 0;
 			}
-			else if (S[i] == 3)
+			else if (Time[i] == 0)
 			{
-				if (CurrState[i] == KEY_UNPRESSED)
-				{
-					S[i] = 0;
-				}
-			}
-			else if (S[i] == 4)
-			{
-				if (CurrState[i] == KEY_UNPRESSED)
-				{
-					S[i] = 0;
-				}
-				else if (Time[i] == 0)
-				{
-					Time[i] = KEY_TIME_REPEAT;
-					Key_Flag[i] |= KEY_REPEAT;
-					S[i] = 4;
-				}
+				Time[i] = KEY_TIME_REPEAT;
+				Key_Flag[i] |= KEY_REPEAT;
+				S[i] = 4;
 			}
 		}
 	}
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -189,7 +189,21 @@ void MX_TIM7_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN TIM7_Init 2 */
-
+  htim7.Instance = TIM7;
+  htim7.Init.Prescaler = 169;
+  htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim7.Init.Period = 9999;
+  htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim7, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE END TIM7_Init 2 */
 
 }
